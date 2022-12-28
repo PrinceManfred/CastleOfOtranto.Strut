3a0833fca529abe3c992613035adfcf25c0dcf89
PrinceManfred <development@castleofotranto.net>
2022-12-28 14:35:44 -0600

    Added extensions and factories

---
+++ src/Mvc/StrutMvcServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CastleOfOtranto.Strut.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class StrutMvcServiceCollectionExtensions
+{
+	public static IServiceCollection AddStrutMvcRequiredParameters(this IServiceCollection services)
+	{
+		services.TryAddEnumerable(
+			ServiceDescriptor.Transient<IApiDescriptionProvider
+				,RequiredParametersApiDescriptionProvider>());
+
+		return services;
+	}
+}
+
---
+++ src/SystemJson/PropertyHasDefaultMapperFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace CastleOfOtranto.Strut.SystemJson;
+
+public static class PropertyHasDefaultMapperFactory
+{
+	public static JsonSerializerOptions? GetMvcJsonSerializerOptions(
+		this IServiceProvider serviceProvider)
+	{
+        using IServiceScope serviceScope = serviceProvider.CreateScope();
+        var jsonOptions = serviceScope.ServiceProvider.GetService<IOptions<Microsoft.AspNetCore.Mvc.JsonOptions>>();
+
+		return jsonOptions?.Value.JsonSerializerOptions;
+    }
+
+    public static JsonSerializerOptions? GetHttpJsonSerializerOptions(
+        this IServiceProvider serviceProvider)
+    {
+        using IServiceScope serviceScope = serviceProvider.CreateScope();
+        var jsonOptions = serviceScope.ServiceProvider.GetService<IOptions<Microsoft.AspNetCore.Http.Json.JsonOptions>>();
+
+        return jsonOptions?.Value.SerializerOptions;
+    }
+
+    public static PropertyHasDefaultMapper CreateWithMvcOptions(IServiceProvider serviceProvider)
+    {
+        var jsonSerializerOptions = serviceProvider.GetMvcJsonSerializerOptions();
+        return new PropertyHasDefaultMapper(jsonSerializerOptions);
+    }
+
+    public static PropertyHasDefaultMapper CreateWithHttpOptions(IServiceProvider serviceProvider)
+    {
+        var jsonSerializerOptions = serviceProvider.GetHttpJsonSerializerOptions();
+        return new PropertyHasDefaultMapper(jsonSerializerOptions);
+    }
+}
+
