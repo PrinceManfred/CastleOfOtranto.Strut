5d23961fe5fe68eed8dc3c32359f4a5753f21fdb
PrinceManfred <development@castleofotranto.net>
2023-01-06 16:05:42 -0600

    Added a cache for common reflection operations

---
+++ src/Strut/TypeInfoCache.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Xml.Linq;
+
+namespace CastleOfOtranto.Strut
+{
+	public class TypeInfoCache
+	{
+		private readonly ConcurrentDictionary<TypeInfoMemberCacheKey,
+            TypeInfoCacheEntry> _memberCache = new();
+
+        private readonly ConcurrentDictionary<TypeInfoParameterCacheKey,
+            TypeInfoCacheEntry> _parameterCache = new();
+
+        #region Member Methods
+        public bool Contains(Type containingType, string memberName)
+		{
+            return Contains(new TypeInfoMemberCacheKey(containingType, memberName));
+		}
+
+        public bool Contains(TypeInfoMemberCacheKey key)
+        {
+            return _memberCache.ContainsKey(key);
+        }
+
+        public bool TryAdd(Type containingType, string memberName, TypeInfoCacheEntry entry)
+        {
+            return TryAdd(new TypeInfoMemberCacheKey(containingType, memberName), entry);
+        }
+
+        public bool TryAdd(TypeInfoMemberCacheKey key, TypeInfoCacheEntry entry)
+        {
+            return _memberCache.TryAdd(key, entry);
+        }
+
+        public bool TryGet(Type containingType, string memberName, out TypeInfoCacheEntry entry)
+        {
+            return TryGet(new TypeInfoMemberCacheKey(containingType, memberName), out entry);
+        }
+
+        public bool TryGet(TypeInfoMemberCacheKey key, out TypeInfoCacheEntry entry)
+        {
+            return _memberCache.TryGetValue(key, out entry);
+        }
+        #endregion
+
+        #region Parameter Methods
+        public bool Contains(Type containingType, string methodName, string parameterName)
+        {
+            return Contains(new TypeInfoParameterCacheKey(containingType, methodName, parameterName));
+        }
+
+        public bool Contains(TypeInfoParameterCacheKey key)
+        {
+            return _parameterCache.ContainsKey(key);
+        }
+
+        public bool TryAdd(Type containingType, string methodName, string parameterName, TypeInfoCacheEntry entry)
+        {
+            return TryAdd(new TypeInfoParameterCacheKey(containingType, methodName, parameterName), entry);
+        }
+
+        public bool TryAdd(TypeInfoParameterCacheKey key, TypeInfoCacheEntry entry)
+        {
+            return _parameterCache.TryAdd(key, entry);
+        }
+
+        public bool TryGet(Type containingType, string methodName, string parameterName, out TypeInfoCacheEntry entry)
+        {
+            return TryGet(new TypeInfoParameterCacheKey(containingType, methodName, parameterName), out entry);
+        }
+
+        public bool TryGet(TypeInfoParameterCacheKey key, out TypeInfoCacheEntry entry)
+        {
+            return _parameterCache.TryGetValue(key, out entry);
+        }
+        #endregion
+    }
+}
+
---
+++ src/Strut/TypeInfoCacheRecords.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace CastleOfOtranto.Strut
+{
+	public readonly record struct TypeInfoCacheEntry(
+		bool IsDefault,
+		NullabilityState NullabilityState);
+
+	public readonly record struct TypeInfoMemberCacheKey(
+		Type ContainingType,
+		string MemberName);
+
+	public readonly record struct TypeInfoParameterCacheKey(
+		Type ContainingType,
+		string MethodName,
+		string ParameterName);
+}
