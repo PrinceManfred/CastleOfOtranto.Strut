d709506e36399e840c2171e4b32d5d9ea61c7b86
PrinceManfred <development@castleofotranto.net>
2023-01-06 22:40:21 -0600

    SwaggerGenOptions are now injected in the service collection

---
+++ src/Swashbuckle/StrutSwashbuckleServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CastleOfOtranto.Strut;
+using CastleOfOtranto.Strut.Swashbuckle;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class StrutSwashbuckleServiceCollectionExtensions
+{
+    public static IServiceCollection AddStrutSwashbuckle(this IServiceCollection services)
+    {
+        services.TryAddSingleton<TypeInfoCache>();
+
+        services.AddOptions<SwaggerGenOptions>().Configure<TypeInfoCache>((o, cache) =>
+        {
+            o.SupportNonNullableReferenceTypes();
+            o.ParameterFilter<RequiredParameterFilter>();
+            o.SchemaFilter<RequiredPropertySchemaFilter>(cache);
+            o.RequestBodyFilter<RequiredRequestBodyFilter>();
+            o.UseAllOfToExtendReferenceSchemas();
+        });
+
+        return services;
+    }
+}
+
