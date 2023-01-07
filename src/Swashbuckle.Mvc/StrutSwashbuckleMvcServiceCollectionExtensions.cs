1e2a3ed9393dd4a55ec32a2c324d0fb1dfc01ee3
PrinceManfred <development@castleofotranto.net>
2023-01-06 22:41:53 -0600

    Add Swashbuckle.Mvc project for easier DI

---
+++ src/Swashbuckle.Mvc/StrutSwashbuckleMvcServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using CastleOfOtranto.Strut;
+using CastleOfOtranto.Strut.Mvc;
+using CastleOfOtranto.Strut.Swashbuckle;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class StrutSwashbuckleMvcServiceCollectionExtensions
+{
+    public static IServiceCollection AddStrutSwashbuckleMvc(this IServiceCollection services)
+    {
+        services.TryAddSingleton<TypeInfoCache>();
+
+        services.AddOptions<MvcOptions>().Configure<TypeInfoCache>((options, cache) =>
+        {
+            options.ModelMetadataDetailsProviders.Add(new StrutValidationMetadataProvider(cache));
+            options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
+        });
+
+        services.AddOptions<SwaggerGenOptions>().Configure<TypeInfoCache>((o, cache) =>
+        {
+            o.SupportNonNullableReferenceTypes();
+            o.ParameterFilter<RequiredParameterFilter>();
+            o.SchemaFilter<RequiredPropertySchemaFilter>(cache);
+            o.RequestBodyFilter<RequiredRequestBodyFilter>();
+            o.UseAllOfToExtendReferenceSchemas();
+        });
+
+        return services;
+    }
+}
