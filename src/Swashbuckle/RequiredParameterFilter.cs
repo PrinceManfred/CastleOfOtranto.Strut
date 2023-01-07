2149cf442266d2220e358dd948e4aed72ef9c1e0
PrinceManfred <development@castleofotranto.net>
2023-01-06 22:40:48 -0600

    Take into account complex model bindings from parameter data

--- src/Swashbuckle/RequiredParameterFilter.cs
+++ src/Swashbuckle/RequiredParameterFilter.cs
@@ -8,19 +8,22 @@ namespace CastleOfOtranto.Strut.Swashbuckle;
 
 public class RequiredParameterFilter : IParameterFilter
 {
-    private readonly NullabilityInfoContext _nullabilityInfoContext = new();
-
     public void Apply(OpenApiParameter parameter, ParameterFilterContext context)
     {
         if (parameter.Required) return;
         if (context.ParameterInfo.ParameterType.IsValueType) return;
         if (context.ParameterInfo.HasDefaultValue) return;
 
-        // TODO: context.PropertyInfo might not be null here if binding complex types from query
-        // we need to process that if it's here.
+        if(context.PropertyInfo is not null)
+        {
+            if (context.PropertyInfo.HasDefault()) return;
+            if (context.PropertyInfo.GetNullabilityState() != NullabilityState.NotNull)
+            {
+                return;
+            }
+        }
 
-        NullabilityInfo nullabilityInfo = _nullabilityInfoContext.Create(context.ParameterInfo);
-        if(nullabilityInfo.WriteState == NullabilityState.NotNull)
+        if(context.ParameterInfo.GetNullabilityState() == NullabilityState.NotNull)
         {
             parameter.Required = true;
         }
