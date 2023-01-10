df6601a7f55057ffc7b9417caffe05fc06edf76b
PrinceManfred <development@castleofotranto.net>
2023-01-10 11:30:25 -0600

    Check for StrutIgnore first, remove any HasDefaultValue extensions.

--- src/Swashbuckle/RequiredParameterFilter.cs
+++ src/Swashbuckle/RequiredParameterFilter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System;
+using System.Data.Common;
 using System.Reflection;
+using System.Reflection.Metadata;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -10,9 +12,6 @@ public class RequiredParameterFilter : IParameterFilter
 {
     public void Apply(OpenApiParameter parameter, ParameterFilterContext context)
     {
-        if (parameter.Required) return;
-        if (context.ParameterInfo.ParameterType.IsValueType) return;
-        if (context.ParameterInfo.HasDefaultValue) return;
         if (context.ParameterInfo.GetCustomAttributes()
             .OfType<StrutIgnoreAttribute>()
             .FirstOrDefault() is not null)
@@ -20,8 +19,17 @@ public class RequiredParameterFilter : IParameterFilter
             return;
         }
 
+        if (parameter.Required) return;
+        if (context.ParameterInfo.ParameterType.IsValueType) return;
+        if (context.ParameterInfo.HasDefaultValue) return;
+        
         if(context.PropertyInfo is not null)
         {
+            if (parameter.Schema.Extensions.ContainsKey(HasDefaultValueExtension.EXTENSION_NAME))
+            {
+                parameter.Schema.Extensions.Remove(HasDefaultValueExtension.EXTENSION_NAME);
+            }
+
             if (context.PropertyInfo.HasDefault()) return;
             if (context.PropertyInfo.GetNullabilityState() != NullabilityState.NotNull)
             {
